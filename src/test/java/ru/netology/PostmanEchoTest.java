37d8acbf4eb2e356a01c9540e6c011c0d7bce086
Sofia Larina <zazaq97@gmail.com>
2020-08-29 21:26:18 +0300

    correct version of test

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -15,6 +15,6 @@ public class PostmanEchoTest {
                 .post("/post")
             .then()
                 .statusCode(200)
-                .body("data", equalTo("data"));
+                .body("data", equalTo("Test data"));
         }
 }
